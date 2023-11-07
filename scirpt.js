cdaa985582591f79884c8a30d5cffa39099bd9bd
MichaelXDXD1 <148601637+MichaelXDXD1@users.noreply.github.com>
2023-11-07 13:24:15 +0700

    Add files via upload

---
+++ scirpt.js
@@ -0,0 +1,16 @@
+function Login() {
+  var done = 0;
+  var username = document.login.username.value;
+  var password = document.login.password.value;
+  if (username == "Mabim" && password == "Mabim") {
+    if (confirm("Apakah anda ingin login?") == true) {
+      window.location.href = "https://github.com/MichaelXDXD1";
+    } else {
+      alert("Login dibatalkan !");
+    }
+  } else {
+    if (done == 0) {
+      alert("Username/Pasword Salah");
+    }
+  }
+}
